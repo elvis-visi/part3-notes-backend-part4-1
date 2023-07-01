4bd65bad1b402519326fbbf07a784710298fe0ea
elvis-visi <sivisevi1@gmail.com>
2023-07-01 19:11:04 +0200

    2. create a new note. Add userid to note, then concatthe added note to the user.

--- models/user.js
+++ models/user.js
@@ -36,4 +36,21 @@ module.exports = User
 /*
 The type of the field is ObjectId that references note-style documents. Mongo does not inherently know that this is a field that references notes, the syntax is purely related to and defined by Mongoose.
 
+
+
+
+
+    You define your Mongoose schema and specify which fields should be unique. In your case, the username field is set to unique: true.
+
+    You add the uniqueValidator plugin to your schema with userSchema.plugin(uniqueValidator).
+
+    When you try to save a document using this schema, Mongoose runs all its usual pre-save validation steps. The uniqueValidator plugin adds an extra step here.
+
+    For each field in the document that is marked as unique, the uniqueValidator plugin checks if there is an existing document in the database with the same value for that field.
+
+    If a duplicate value is found, the plugin stops the save operation and returns a validation error.
+
+    If no duplicate values are found, the save operation proceeds as normal.
+
+
 */

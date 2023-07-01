fdcbe2f482c6eb20a1ed659d4cdd1a0d3de2a9b7
elvis-visi <sivisevi1@gmail.com>
2023-07-01 19:28:51 +0200

    4. populate the user ids in notes with specific info about those users.

--- models/user.js
+++ models/user.js
@@ -53,4 +53,22 @@ The type of the field is ObjectId that references note-style documents. Mongo do
     If no duplicate values are found, the save operation proceeds as normal.
 
 
+
+    It's important to understand that the database does not know that the ids stored in the user field of notes reference documents in the user collection.
+
+The functionality of the populate method of Mongoose is based on the fact that we have defined "types" to the references in the Mongoose schema with the ref option:
+
+const noteSchema = new mongoose.Schema({
+  content: {
+    type: String,
+    required: true,
+    minlength: 5
+  },
+  important: Boolean,
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User'
+  }
+})
+
 */
